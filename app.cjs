ad43bbf066b9e27d0634c526ffa53c8edc2699b0
jimmantha <jimloy.22@ichat.sp.edu.sg>
2024-05-29 12:49:36 +0800

    make gauge replicate base on zones store. Create test button to update gauge

--- app.cjs
+++ app.cjs
@@ -46,7 +46,7 @@ mongoose.connect('mongodb+srv://pleasepeople123:VfLWNiTsHAUOZjkY@cluster0.75o7ls
 
 
 // Connect to the MQTT broker
-const client = mqtt.connect('mqtt://172.23.18.169:1883');
+const client = mqtt.connect('mqtt://localhost:1883');
 
 // Create a schema for the sensor data
 const sensorDataSchema = new mongoose.Schema({
