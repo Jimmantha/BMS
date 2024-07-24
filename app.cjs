ad86461b2164ad9e343e6937f1d6eeb887b17c7d
Jimmantha <jimloy.22@ichat.sp.edu.sp>
2024-07-24 11:22:28 +0800

    chore: chart fix

--- app.cjs
+++ app.cjs
@@ -49,15 +49,11 @@ async function fetchFloorDetails() {
         console.error('Error:', err);
     }
 }
-mongoose.connect('mongodb://admin:pass123@localhost:27017/myDatabase', {
-    authSource: 'admin'
-}).then(() => {
-    console.log('connected to db');
-}).catch(err => console.log(err));
+mongoose.connect('mongodb+srv://pleasepeople123:VfLWNiTsHAUOZjkY@cluster0.75o7lsi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 //172.23.17.115:1883 dev mqtt broker address
 //172.23.16.143:1883 dev mqtt broker address
 // Connect to the MQTT broker
-const client = mqtt.connect('mqtt://13.213.207.72:1883');
+const client = mqtt.connect('mqtt://localhost:1883');
 
 // Create a schema for the sensor data
 const sensorDataSchema = new mongoose.Schema({
@@ -290,17 +286,19 @@ client.on('message', async (topic, message) => {
         // Save the sensor data to MongoDB
         var currenttime = new Date();
 
-        if (currenttime - sensorSaveTime > 60000) { //300000ms = 5 minutes
-            await newSensorData.save().then(() => {
+        if (currenttime - sensorSaveTime > 5000) { //300000ms = 5 minutes
+            await newSensorData.save().then(async () => {
                 sensorSaveTime = new Date();
+                data = await getSensorData();
+                io.emit('sensorData', { sensorData: data });
             });
         } else if (sensorSaveTime == undefined) {
             await newSensorData.save().then(() => {
                 sensorSaveTime = new Date();
             });
         }
-        data = await getSensorData();
-        io.emit('sensorData', { sensorData: data });
+
+
     }
     if (topic == 'energyReadings') {
         data = JSON.parse(message);
